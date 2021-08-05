a0d94e4a22aa6f65049f8c6622a76210e494beab
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2021-08-05 14:23:31 -0300

    Implements first version off Vector add method.

--- src/main/java/io/github/ricardosander/datastructures/Vector.java
+++ src/main/java/io/github/ricardosander/datastructures/Vector.java
@@ -1,7 +1,14 @@
 package io.github.ricardosander.datastructures;
 
 public class Vector {
+
+  private boolean isEmpty = true;
+
   public boolean isEmpty() {
-    return false;
+    return isEmpty;
+  }
+
+  public void add(String element) {
+    isEmpty = false;
   }
 }
---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+
+  @org.junit.jupiter.api.Test
+  void test() {
+    assertTrue(true);
+  }
+}
